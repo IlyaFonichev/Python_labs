8d2595f0b01ecf02e2bd82bb1f580f6f86526e3f
GlavnayaKotymba <90211263+IlyaFonichev@users.noreply.github.com>
2023-11-17 19:18:10 +0300

    Исправление ошибок в Задаче №2

---
+++ main.py
@@ -0,0 +1,19 @@
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    with open(INPUT_FILENAME) as input_, open(OUTPUT_FILENAME, 'w') as output_:
+        result = [row for row in csv.DictReader(input_)]
+        json.dump(result, output_, indent=4)
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
